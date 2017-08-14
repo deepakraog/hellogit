dd5a1568c0e72b31fd951f5e0def09e69db92d39
deepak.gaikwad <gaikwad.dcg@gmail.com>
2017-08-14 23:56:42 +0530

    for loop changed

--- HelloMiddleEarth/src/org/drg/hellomiddleearth/HelloMiddleEarth.java
+++ HelloMiddleEarth/src/org/drg/hellomiddleearth/HelloMiddleEarth.java
@@ -4,11 +4,13 @@ public class HelloMiddleEarth {
 
 	public static void main(String[] args) {
 
-		String[] names = {"Bilbo","Frodo","Aragon","Legolas","Gandalf"};
+		String[] names = {"Bilbo","Frodo","Aragon","Legolas","Gandalf","Boromir","Faramir"};
 		
 		//print Greetings
 		for(int i = 0;i<names.length;i++){
 			System.out.format("Hello, %s!\n", names[i]);
+			System.out.println("How are you doing Today?");
+			
 		}
 	}
 
